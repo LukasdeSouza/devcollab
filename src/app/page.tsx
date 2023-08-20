bedac9de58e5e23dfb945100bfdf391a00b7bba1
LukasdeSouza <lucas@bestdeal.com.br>
2023-08-20 19:27:39 -0300

    feature: prices section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ContentSection from '@/components/content'
 import CTASection from '@/components/cta'
 import HeroSection from '@/components/hero'
+import PriceSection from '@/components/price'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <CTASection />
       <ContentSection />
+      <PriceSection />
     </>
   )
 }
